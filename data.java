dedfdde8650c6df6e1cf8b0b9fd6bcfeefc2a8e4
Mahmudul Hasan Shovon <101075577+im-mahmudul@users.noreply.github.com>
2024-06-04 20:56:20 +0600

    Add files via upload

---
+++ data.java
@@ -0,0 +1,8 @@
+public class data{
+    public static void main( String [] args ){
+        int n = 48;
+        int m = 67;
+        int result = n * m;
+        System.out.println(result);
+    }
+}
