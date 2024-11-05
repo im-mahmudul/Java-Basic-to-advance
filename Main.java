a16c53353183739e8faed288a31f7056f6ede23b
Mahmudul Hasan Shovon <101075577+im-mahmudul@users.noreply.github.com>
2024-11-05 18:31:27 +0600

    Main.java

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+    byte num = 75;
+    short num2 = 7587;
+    long num3 = 58234234;
+    int my_num = 58;
+    double d1 = 4.23232;    //ekhane ekta fractional value niyechi
+    float f1 = 8.9f;
+    char ch = 'A';
+    boolean result1 = true;
+    boolean result2 = false;
+    String myString = "Shovon";
+    int [] arr1 = {11,12,13,14};
+
+    System.out.println("Hello world");
+    System.out.print("Hello world");
+    System.out.println();
+    System.out.printf("Formated val = %.2f\n", d1);
+    }
+}
--- data.java
+++
@@ -1,8 +0,0 @@
-public class data{
-    public static void main( String [] args ){
-        int n = 48;
-        int m = 67;
-        int result = n * m;
-        System.out.println(result);
-    }
-}
