1852e5ef748073f8cb0a0bbe8f0730c2ddfebcd8
Mahmudul Hasan Shovon <101075577+im-mahmudul@users.noreply.github.com>
2024-11-05 20:42:48 +0000

    practice

---
+++ day32.java
@@ -0,0 +1,7 @@
+public class day32 {
+    public static void main(String[] args) {
+        int u = 8;
+        System.out.println(5 == 3);
+        System.out.println(u);
+    }
+}
---
+++ pc.java
@@ -0,0 +1,10 @@
+public class pc {
+        public static void main(String[] args) {
+            System.out.println("Hello world");
+            System.out.println("Hello worlds");
+            System.out.println("Hello worldss");
+
+            int a = 5;
+            
+    }
+}
