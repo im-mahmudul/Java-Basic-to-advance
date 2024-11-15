022f7b975f2d157660ca40ac99ae9e3b7431fe85
Mahmudul Hasan Shovon <101075577+im-mahmudul@users.noreply.github.com>
2024-11-15 19:52:41 +0600

    Add files via upload

--- pattern.java
+++ pattern.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
 public class pattern {
-    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number you want to print: ");
+        int num = sc.nextInt();
+         for(int i = 1; i <= num; i++){
+            for(int j = 1; j <= num; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+         }
+    } 
 }
